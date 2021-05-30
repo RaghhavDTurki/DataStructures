f9f84cfa87e408c814370962a4afe66ac19e728e
RaghhavDTurki <raghhavdturki@gmail.com>
2021-05-30 09:08:19 +0530

    Updating the repository

--- Trees/C++/BST.cpp
+++ Trees/C++/BST.cpp
@@ -1,32 +1,169 @@
-#include <bits/stdc++.h>
-#include <complex>
-#include <queue>
-#include <set>
-#include <unordered_set>
-#include <list>
-#include <chrono>
-#include <random>
+// C++ program to demonstrate insertion
+// in a BST recursively.
 #include <iostream>
-#include <algorithm>
-#include <cmath>
-#include <string>
-#include <vector>
-#include <map>
-#include <unordered_map>
-#include <stack>
-#include <iomanip>
-#include <fstream>
-
 using namespace std;
+ 
+class BST
+{
+    int data;
+    BST *left, *right;
+ 
+public:
+    // Default constructor.
+    BST();
+ 
+    // Parameterized constructor.
+    BST(int);
+ 
+    // Insert function.
+    BST* Insert(BST*, int);
+ 
+    // Inorder traversal.
+    void Inorder(BST*);
 
-#define mem1(a) memset(a,-1,sizeof(a))
-#define mem0(a) memset(a,0,sizeof(a))
-#define ln "\n"
-#define ll long long int
+    // Find Minimun value node in the BST
+    BST* minValueNode(BST*);
+
+    // Delete the node with given value
+    BST* deleteNode(BST*, int);
+};
+ 
+// Default Constructor definition.
+BST ::BST()
+    : data(0)
+    , left(NULL)
+    , right(NULL)
+{
+}
+ 
+// Parameterized Constructor definition.
+BST ::BST(int value)
+{
+    data = value;
+    left = right = NULL;
+}
+ 
+// Insert function definition.
+BST* BST ::Insert(BST* root, int value)
+{
+    if (!root)
+    {
+        // Insert the first node, if root is NULL.
+        return new BST(value);
+    }
+ 
+    // Insert data.
+    if (value > root->data)
+    {
+        // Insert right node data, if the 'value'
+        // to be inserted is greater than 'root' node data.
+ 
+        // Process right nodes.
+        root->right = Insert(root->right, value);
+    }
+    else
+    {
+        // Insert left node data, if the 'value'
+        // to be inserted is greater than 'root' node data.
+ 
+        // Process left nodes.
+        root->left = Insert(root->left, value);
+    }
+ 
+    // Return 'root' node, after insertion.
+    return root;
+}
+ 
+// Inorder traversal function.
+// This gives data in sorted order.
+void BST ::Inorder(BST* root)
+{
+    if (!root) {
+        return;
+    }
+    Inorder(root->left);
+    cout << root->data << " ";
+    Inorder(root->right);
+}
+
+BST* BST ::minValueNode(BST* root)
+{
+    BST* current = root;
+    while(current && current->left != NULL)
+    {
+        current = current->left;
+    }
+    return current;
+}
 
 
+BST* BST ::deleteNode(BST* root, int key)
+{
+    // base case
+    if (root == NULL)
+        return root;
+ 
+    // If the key to be deleted
+    // is smaller than the root's
+    // key, then it lies in left subtree
+    if (key < root->data)
+        root->left = deleteNode(root->left, key);
+ 
+    // If the key to be deleted
+    // is greater than the root's
+    // key, then it lies in right subtree
+    else if (key > root->data)
+        root->right = deleteNode(root->right, key);
+ 
+    // if key is same as root's key,
+    // then This is the node
+    // to be deleted
+    else {
+        // node with only one child or no child
+        if (root->left == NULL) {
+            BST* temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if (root->right == NULL) {
+            BST* temp = root->left;
+            free(root);
+            return temp;
+        }
+ 
+        // node with two children:
+        // Get the inorder successor
+        // (smallest in the right subtree)
+        BST* temp = minValueNode(root->right);
+ 
+        // Copy the inorder
+        // successor's content to this node
+        root->data = temp->data;
+ 
+        // Delete the inorder successor
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
 
+
+ 
+// Driver code
 int main()
 {
-
+    BST b, *root = NULL;
+    root = b.Insert(root, 50);
+    b.Insert(root, 30);
+    b.Insert(root, 20);
+    b.Insert(root, 40);
+    b.Insert(root, 70);
+    b.Insert(root, 60);
+    b.Insert(root, 80);
+ 
+    b.Inorder(root);
+    cout << endl;
+    cout << "Delete 40" << endl;
+    b.deleteNode(root,40);
+    b.Inorder(root);
+    return 0;
 }
