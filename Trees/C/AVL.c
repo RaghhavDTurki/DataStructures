2c96bbdd536cf8ff64f5ca6d52188c994807cb5d
RaghhavDTurki <raghhavdturki@gmail.com>
2021-07-18 08:21:29 +0530

    Updating the repository

---
+++ Trees/C/AVL.c
@@ -0,0 +1,90 @@
+// C program to implement AVL Tree
+#include <stdio.h>
+#include <stdlib.h>
+
+// An AVL tree node
+struct Node
+{
+    int key;
+    struct Node *left;
+    struct Node *right;
+    int height;
+};
+
+
+// A utility function to get the height of the tree
+int height(struct Node *N)
+{
+    if (N == NULL)
+        return 0;
+    return N->height;
+}
+
+// A utility function to get maximum of two integers
+int max(int a, int b)
+{
+    return (a > b) ? a : b;
+}
+
+/* Helper function that allocates a new node with the given key and
+    NULL left and right pointers. */
+struct Node* newNode(int key)
+{
+    struct Node* node = (struct Node*)
+                        malloc(sizeof(struct Node));
+    node->key   = key;
+    node->left   = NULL;
+    node->right  = NULL;
+    node->height = 1;  // new node is initially added at leaf
+    return (node);
+}
+
+
+// A utility function to right rotate subtree rooted with y
+// See the diagram given above.
+struct Node *rightRotate(struct Node *y)
+{
+    struct Node *x = y->left;
+    struct Node *T2 = x->right;
+
+    // Perform rotation
+    x->right = y;
+    y->left = T2;
+
+    // Update heights
+    y->height = max(height(y->left), height(y->right)) + 1;
+    x->height = max(height(x->left), height(x->right)) + 1;
+
+    // Return new root
+    return x;
+}
+
+
+// A utility function to left rotate subtree rooted with x
+// See the diagram given above.
+struct Node *leftRotate(struct Node *x)
+{
+    struct Node *y = x->right;
+    struct Node *T2 = y->left;
+
+    // Perform rotation
+    y->left = x;
+    x->right = T2;
+
+    //  Update heights
+    x->height = max(height(x->left), height(x->right)) + 1;
+    y->height = max(height(y->left), height(y->right)) + 1;
+
+    // Return new root
+    return y;
+}
+
+
+// Get Balance factor of node N
+int getBalance(struct Node *N)
+{
+    if (N == NULL)
+        return 0;
+    return height(N->left) - height(N->right);
+}
+
--- Trees/C/BST.c
+++ Trees/C/BST.c
@@ -3,12 +3,12 @@
 // search tree.
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 struct node {
     int key;
     struct node *left, *right;
 };
- 
+
 // A utility function to create a new BST node
 struct node* newNode(int item)
 {
@@ -17,7 +17,7 @@ struct node* newNode(int item)
     temp->left = temp->right = NULL;
     return temp;
 }
- 
+
 // A utility function to do inorder traversal of BST
 void inorder(struct node* root)
 {
@@ -37,13 +37,13 @@ struct node* insert(struct node* node, int key)
     /* If the tree is empty, return a new node */
     if (node == NULL)
         return newNode(key);
- 
+
     /* Otherwise, recur down the tree */
     if (key < node->key)
         node->left = insert(node->left, key);
     else if (key > node->key)
         node->right = insert(node->right, key);
- 
+
     /* return the (unchanged) node pointer */
     return node;
 }
@@ -57,14 +57,14 @@ struct node* insert(struct node* node, int key)
 struct node* minValueNode(struct node* node)
 {
     struct node* current = node;
- 
+
     /* loop down to find the leftmost leaf */
     while (current && current->left != NULL)
         current = current->left;
- 
+
     return current;
 }
- 
+
 /* Given a binary search tree
    and a key, this function
    deletes the key and
@@ -74,19 +74,19 @@ struct node* deleteNode(struct node* root, int key)
     // base case
     if (root == NULL)
         return root;
- 
+
     // If the key to be deleted
     // is smaller than the root's
     // key, then it lies in left subtree
     if (key < root->key)
         root->left = deleteNode(root->left, key);
- 
+
     // If the key to be deleted
     // is greater than the root's
     // key, then it lies in right subtree
     else if (key > root->key)
         root->right = deleteNode(root->right, key);
- 
+
     // if key is same as root's key,
     // then This is the node
     // to be deleted
@@ -102,16 +102,16 @@ struct node* deleteNode(struct node* root, int key)
             free(root);
             return temp;
         }
- 
+
         // node with two children:
         // Get the inorder successor
         // (smallest in the right subtree)
         struct node* temp = minValueNode(root->right);
- 
+
         // Copy the inorder
         // successor's content to this node
         root->key = temp->key;
- 
+
         // Delete the inorder successor
         root->right = deleteNode(root->right, temp->key);
     }
@@ -131,28 +131,28 @@ int main()
     root = insert(root, 50);
     root = insert(root, 30);
     root = insert(root, 20);
-    root = insert(root, 40);
+    // root = insert(root, 40);
     root = insert(root, 70);
     root = insert(root, 60);
     root = insert(root, 80);
- 
+
     printf("Inorder traversal of the given tree \n");
     inorder(root);
- 
-    printf("\nDelete 20\n");
-    root = deleteNode(root, 20);
-    printf("Inorder traversal of the modified tree \n");
-    inorder(root);
- 
+
+    // printf("\nDelete 20\n");
+    // root = deleteNode(root, 20);
+    // printf("Inorder traversal of the modified tree \n");
+    // inorder(root);
+
     printf("\nDelete 30\n");
     root = deleteNode(root, 30);
     printf("Inorder traversal of the modified tree \n");
     inorder(root);
- 
+
     printf("\nDelete 50\n");
     root = deleteNode(root, 50);
     printf("Inorder traversal of the modified tree \n");
     inorder(root);
- 
+
     return 0;
 }
